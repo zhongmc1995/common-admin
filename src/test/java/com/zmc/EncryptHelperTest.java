ee04220a497ebd1e186b8d018aafa990db383ecf
zhongmc <zhongmc_me@163.com>
2017-06-29 19:16:30 +0800

    整合shiro

---
+++ src/test/java/com/zmc/EncryptHelperTest.java
@@ -0,0 +1,22 @@
+package com.zmc;
+
+import com.zmc.common.entity.User;
+import com.zmc.utils.EncryptHelper;
+import org.junit.Test;
+
+/**
+ * Created by zhongmc on 2017/6/29.
+ */
+public class EncryptHelperTest {
+    @Test
+    public void encryptTest(){
+        EncryptHelper encryptHelper = new EncryptHelper();
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+        encryptHelper.encrypt(user);
+        System.out.println(user);
+        System.out.println(user.getPassword());
+        System.out.println(user.getSalt());
+    }
+}
